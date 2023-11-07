ce389a3440f59380d320c6604c04cec10b1f1595
nat662434 <117282786+nat662434@users.noreply.github.com>
2023-11-07 19:55:22 +0100

    Create ariketa1-1.php

---
+++ 1_ebaluaketa/Ejercicios - repaso/Ariketa1/ariketa1-1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Ariketa 1 - 1</title>
+</head>
+<body>
+    <?php 
+    // aldagaia sortu pantailatik biztaratzeko
+    $zenbaki = 24;
+    // echo egin pantailatik biztaratzeko
+    echo "Zenbakia: " . $zenbaki;
+
+
+    ?>
+    
+</body>
+</html>
+
